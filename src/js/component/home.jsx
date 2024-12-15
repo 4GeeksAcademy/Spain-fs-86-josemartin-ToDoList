dc88ef56ff0dfd20496b963eb2b20046c62aebd3
josemartinbm99 <josemartinbm99@gmail.com>
2024-12-15 18:02:22 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const reyesList = [{
+	who: "Hans",
+	what: "termomix",
+	where: "Saspikalea",
+  },  {
+	who: "My brother",
+	what: "pan",
+	where: "his new house",
+  },  {
+	who: "David",
+	what: "Red Bull",
+	where: "Benalmádena",
+  }]
+  
+	
+  
+  function Home() {
+  
+	const [state, setState] = useState(reyesList)
+	const [who, setWho] = useState("")
+	const [what, setWhat] = useState("")
+	const [where, setWhere] = useState("")
+  
+  
+	const agregar = () => {
+	  setState([...state, {who: who, what: what, where: where}])
+	  setWho("")
+	  setWhat("")
+	  setWhere("")
+	}
 
-//create your first component
-const Home = () => {
+	const eliminarItem = (listItemIndex) => {
+	  const list = state.filter((listItem, index) => {
+		return index != listItemIndex
+	  })
+	  setState(list)
+	}
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
-export default Home;
+	  <>
+	  	<p>Dear Santa Claus...</p>
+	   <input type="text" placeholder="who" value={who}  onChange={(e) => {
+		 setWho(e.target.value)
+	   }}></input>
+	   <input type="text" placeholder="what" value={what} onChange={(e) => {
+		setWhat(e.target.value)
+	   }}></input>
+	   <input type="text" placeholder="where" value={where} onChange={(e) => {
+		setWhere(e.target.value)
+	   }}></input>
+	   <button onClick={agregar}>Add</button>
+	   <ul>
+		{state.map((item, index) => {
+		  return (<>
+			<li>
+			  {item.who + " wants a " + item.what + " in " + item.where} <button onClick={() => eliminarItem(index)}>Remove</button> 
+			</li>
+		  </>)
+		})}
+	   </ul>
+	   <p>{state.length + " item left"}</p>
+	  </>
+	)
+  }
+  
+  export default Home;
